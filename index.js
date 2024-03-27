cae10ab7ab63330f1731362bffeddbdf9d2313c9
mickeymond <33749130+mickeymond@users.noreply.github.com>
2024-03-27 11:28:50 +0000

    Making port dynamic

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use('/recipes', recipesRoutes);
 await mongoose.connect(process.env.MONGO_URI);
 
 // Listen for incoming requests
-app.listen(4000, () => {
-    console.log('Express app is running!');
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Express app is running on port ${port}!`);
 });
